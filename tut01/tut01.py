25d69d33ef0bdb0c65d0736b8f4cfe450a3171b5
Anand Kumar <89066810+Sineme01@users.noreply.github.com>
2022-08-26 18:09:52 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,5 @@
-def factorial(int x):
-    
-
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+def factorial(num):
+    return 1 if (num==1 or num==0) else num * factorial(num - 1)
+number = int(input("Enter the number: "))
+print("Factorial of",number,"is",
+factorial(number))
