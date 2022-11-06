5cb29ea5ef0f3cfbb76e2719a1575a32e1ac877e
Anand Kumar <anandkr7658@gmail.com>
2022-11-07 00:33:29 +0530

    final submission with output file

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,4 +1,3 @@
-from platform import python_version
 import csv
 from openpyxl import workbook, load_workbook
 from openpyxl.utils import get_column_letter as l_g
@@ -6,14 +5,14 @@ import pandas as pd
 from openpyxl.styles.borders import Border
 from openpyxl.styles import borders
 from datetime import datetime
-StartTime = datetime.now()
+start_time = datetime.now()
 
 # Help https://youtu.be/H37f_x4wAC0
 
 
 def octant_longest_subsequence_count_with_range():
     # use pandas library to read data
-    DataFrame = pd.read_excel(
+    data_frame = pd.read_excel(
         "input_octant_longest_subsequence_with_range.xlsx")
 
     # use load_workbook library to work with excel
@@ -21,51 +20,51 @@ def octant_longest_subsequence_count_with_range():
         "input_octant_longest_subsequence_with_range.xlsx")
 
     # work with active or current sheet
-    w_sheet = work_book.active
-    len_frame = len(DataFrame['Time'].tolist())
-    time_list = DataFrame['Time'].tolist()
+    work_sheet = work_book.active
+    len_frame = len(data_frame['Time'].tolist())
+    time_list = data_frame['Time'].tolist()
     # store u,v,w in list
-    list_u1 = DataFrame['U'].tolist()
-    list_v1 = DataFrame['V'].tolist()
-    list_w1 = DataFrame['W'].tolist()
+    list_u1 = data_frame['U'].tolist()
+    list_v1 = data_frame['V'].tolist()
+    list_w1 = data_frame['W'].tolist()
     c = [list_u1, list_v1, list_w1]
 
     # calculate mean of u,v,w
-    mean_u = DataFrame['U'].mean()
-    mean_v = DataFrame['V'].mean()
-    mean_w = DataFrame['W'].mean()
+    mean_u = data_frame['U'].mean()
+    mean_v = data_frame['V'].mean()
+    mean_w = data_frame['W'].mean()
 
     row0 = ["U Avg", "V Avg", "W Avg"]
     for i in range(3):
-        w_sheet[l_g(i+5)+"1"] = row0[i]
+        work_sheet[l_g(i+5)+"1"] = row0[i]
 
     mean = [mean_u, mean_v, mean_w]
     for i in range(3):
-        w_sheet[l_g(i+5)+"2"] = mean[i]
+        work_sheet[l_g(i+5)+"2"] = mean[i]
 
     b_list = ["U'=U-Uavg", "V'=V-Vavg", "W'=W-Wavg"]
     for i in range(3):
-        w_sheet[l_g(i+8)+"1"] = b_list[i]
+        work_sheet[l_g(i+8)+"1"] = b_list[i]
 
     for i in range(3):
         for j in range(1, len_frame+1):
-            w_sheet[l_g(i+8)+str(j+1)] = c[i][j-1]-mean[i]
+            work_sheet[l_g(i+8)+str(j+1)] = c[i][j-1]-mean[i]
 
     dint = {'+++': "+1", "++-": "-1", "-++": "+2", "-+-": "-2",
             "--+": "+3", "---": "-3", "+-+": "+4", "+--": "-4"}
 
-    w_sheet[l_g(11)+"1"] = "Octant"
+    work_sheet[l_g(11)+"1"] = "Octant"
 
     # calculating oct_ant value
     octant_list = []
     for i in range(1, len_frame+1):
         xim = ""
         for j in range(8, 11):
-            if (w_sheet[l_g(j)+str(i+1)].value >= 0):
+            if (work_sheet[l_g(j)+str(i+1)].value >= 0):
                 xim += '+'
             else:
                 xim += '-'
-        w_sheet[l_g(11)+str(i+1)] = dint[xim]
+        work_sheet[l_g(11)+str(i+1)] = dint[xim]
         octant_list.append(dint[xim])
 
     gint = ['Count', "Longest Subsquence Length", "count"]
@@ -94,37 +93,37 @@ def octant_longest_subsequence_count_with_range():
 
             lount = 1
 
-    rowCur = 2
+    row_curr = 2
     # creating table 1 title
     for k in range(3):
-        w_sheet[l_g(13+k)+str(rowCur)] = gint[k]
+        work_sheet[l_g(13+k)+str(row_curr)] = gint[k]
     # cretaing table 2 title
     for k in range(3):
-        w_sheet[l_g(17+k)+str(rowCur)] = gint[k]
+        work_sheet[l_g(17+k)+str(row_curr)] = gint[k]
 
     # updating table 1
     for kol in range(8):
 
-        w_sheet[l_g(13)+str(kol+3)] = fint[kol]
-        w_sheet[l_g(13+1)+str(kol+3)] = longest[kol]
-        w_sheet[l_g(13+2)+str(kol+3)] = countmam[kol]
+        work_sheet[l_g(13)+str(kol+3)] = fint[kol]
+        work_sheet[l_g(13+1)+str(kol+3)] = longest[kol]
+        work_sheet[l_g(13+2)+str(kol+3)] = countmam[kol]
 
     # updating table 2 in desired way
     curr_row = 3
     for kol in range(8):
 
-        w_sheet[l_g(17)+str(curr_row)] = fint[kol]
-        w_sheet[l_g(17+1)+str(curr_row)] = longest[kol]
-        w_sheet[l_g(17+2)+str(curr_row)] = countmam[kol]
+        work_sheet[l_g(17)+str(curr_row)] = fint[kol]
+        work_sheet[l_g(17+1)+str(curr_row)] = longest[kol]
+        work_sheet[l_g(17+2)+str(curr_row)] = countmam[kol]
         curr_row += 1
         for k in range(3):
-            w_sheet[l_g(17+k)+str(curr_row)] = lint[k]
+            work_sheet[l_g(17+k)+str(curr_row)] = lint[k]
         curr_row += 1
         # pushong all time ranges
         for lol in range(len(timestore[kol])):
-            w_sheet[l_g(18)+str(curr_row)] = timestore[kol][lol] - \
+            work_sheet[l_g(18)+str(curr_row)] = timestore[kol][lol] - \
                 ((longest[kol]-1)/100)
-            w_sheet[l_g(19)+str(curr_row)] = timestore[kol][lol]
+            work_sheet[l_g(19)+str(curr_row)] = timestore[kol][lol]
             curr_row += 1
 
     # creating border design
@@ -133,12 +132,12 @@ def octant_longest_subsequence_count_with_range():
     thin = Border(left=border_1, right=border_0, bottom=border_0, top=border_0)
 
     # drawing border for table 1
-    for row in w_sheet.iter_rows(min_row=rowCur, min_col=13, max_row=rowCur+8, maxx_coll=13+2):
+    for row in work_sheet.iter_rows(min_row=row_curr, min_col=13, max_row=row_curr+8, max_col=13+2):
         for cell in row:
             cell.border = thin
 
     # drawing border for table 2
-    for row in w_sheet.iter_rows(min_row=2, min_col=17, max_row=curr_row-1, maxx_coll=17+2):
+    for row in work_sheet.iter_rows(min_row=2, min_col=17, max_row=curr_row-1, max_col=17+2):
         for cell in row:
             cell.border = thin
 
@@ -147,6 +146,8 @@ def octant_longest_subsequence_count_with_range():
 
 
 octant_longest_subsequence_count_with_range()
+
+
 # This shall be the last lines of the code.
 end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - StartTime))
+print('Duration of Program Execution: {}'.format(end_time - start_time))
