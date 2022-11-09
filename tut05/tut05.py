b43b3c65ac5e0874034c1a6c8ae0d43003f7a13d
Anand Kumar <anandkr7658@gmail.com>
2022-11-09 23:40:22 +0530

    Update tut05.py

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -180,17 +180,6 @@ def octant_range_names(mod=5000):
     work_book.save("octant_output_ranking_excel.xlsx")
 
 
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
 mod=5000 
 octant_range_names(mod)
 
